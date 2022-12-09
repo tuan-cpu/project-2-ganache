dfafb95a0ddfc31e30936e728c1e55dc553eb950
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2022-12-09 20:20:52 +0700

    finish sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,7 +2,7 @@ import { useState, React, useContext, useEffect } from "react";
 import { TransactionContext } from "../context/TransactionContext";
 import { AiOutlineCheckCircle, AiOutlineExclamationCircle } from "react-icons/ai";
 import { BsCircle } from "react-icons/bs";
-import { validNumberOfCharacter, validLowerCharacter, validUpperCharacter, validNumber, validSpecialCharacter } from "../hooks/regex";
+import { validNumberOfCharacter, validLowerCharacter, validUpperCharacter, validNumber, validSpecialCharacter, validEmail } from "../hooks/regex";
 const Input = ({ placeholder, name, type, value, handleChange }) => (
     <input
         placeholder={placeholder}
@@ -22,11 +22,14 @@ const CircleIcon = ({ currentState }) => {
 const SignUp = () => {
     const { signUpFormData, handleSignUp } = useContext(TransactionContext);
     const [passwordState, setPasswordState] = useState({ lower: 0, upper: 0, special: 0, number: 0, char: 0 });
+    const [nameState,setNameState] = useState({first:true,last:true});
+    const [emailState,setEmailState] = useState(true);
     const handleSubmit = (e) => {
         const { firstname, lastname, email, password } = signUpFormData;
         e.preventDefault();
         console.log(signUpFormData);
-        if (!firstname || !lastname || !email || !password) return;
+        if(!firstname) setNameState((prevState)=>({...prevState,first:false}));
+        if(!lastname) setNameState((prevState)=>({...prevState,last:false}));
         if (validNumberOfCharacter.test(password)) setPasswordState((prevState) => ({ ...prevState, char: 1 }));
         else setPasswordState((prevState) => ({ ...prevState, char: -1 }))
         if (validUpperCharacter.test(password)) setPasswordState((prevState) => ({ ...prevState, upper: 1 }));
@@ -37,6 +40,7 @@ const SignUp = () => {
         else setPasswordState((prevState) => ({ ...prevState, special: -1 }))
         if (validNumber.test(password)) setPasswordState((prevState) => ({ ...prevState, number: 1 }));
         else setPasswordState((prevState) => ({ ...prevState, number: -1 }))
+        if(!validEmail.test(email)) setEmailState(false);
     }
     useEffect(() => {
         console.log(passwordState)
@@ -49,8 +53,26 @@ const SignUp = () => {
                 <div className="p-5 sm:w-[32rem] w-full flex flex-col items-center blue-glassmorphism">
                     <div>
                         <Input placeholder='First name' name='firstname' type='text' handleChange={handleSignUp} />
+                        {!nameState.first?(
+                            <div className="flex items-center gap-[10px]">
+                                <AiOutlineExclamationCircle fontSize={17} color='#ff0000'/>
+                                <p className="text-[#ff0000] text-sm">This field must not be blank!</p>
+                            </div>
+                        ):""}
                         <Input placeholder='Last name' name='lastname' type='text' handleChange={handleSignUp} />
+                        {!nameState.last?(
+                            <div className="flex items-center gap-[10px]">
+                                <AiOutlineExclamationCircle fontSize={17} color='#ff0000'/>
+                                <p className="text-[#ff0000] text-sm">This field must not be blank!</p>
+                            </div>
+                        ):""}
                         <Input placeholder='Email' name='email' type='text' handleChange={handleSignUp} />
+                        {!emailState?(
+                            <div className="flex items-center gap-[10px]">
+                                <AiOutlineExclamationCircle fontSize={17} color='#ff0000'/>
+                                <p className="text-[#ff0000] text-sm">Email invalid!</p>
+                            </div>
+                        ):""}
                         <Input placeholder='Password' name='password' type='password' handleChange={handleSignUp} />
                         <div className="flex items-center gap-[10px]">
                             <CircleIcon currentState={passwordState.char} />
--- src/hooks/regex.jsx
+++ src/hooks/regex.jsx
@@ -4,3 +4,4 @@ export const validLowerCharacter = /^.*(?=.*[a-z]).*$/;
 export const validUpperCharacter = /^.*(?=.*[A-Z]).*$/;
 export const validNumber = /^.*(?=.*\d).*$/;
 export const validSpecialCharacter = /^.*(?=.*[!#$%&?@ "]).*$/;
+export const validEmail = /^.*[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}.*$/
