4979ef7a488f8c7a80fef02a0984b74bcd7f8d80
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2023-02-21 09:51:13 +0700

    finish

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -88,10 +88,10 @@ const Welcome = () => {
                         </div>
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder='Address To' name='addressTo' type='text' handleChange={handleChange} />
-                        <Input placeholder='Amount(ETH)' name='amount' type='number' handleChange={handleChange} />
-                        <Input placeholder='Keyword(Gif)' name='keyword' type='text' handleChange={handleChange} />
-                        <Input placeholder='Enter Message' name='message' type='text' handleChange={handleChange} />
+                        <Input placeholder='Địa chỉ nhận' name='addressTo' type='text' handleChange={handleChange} />
+                        <Input placeholder='Số lượng(ETH)' name='amount' type='number' handleChange={handleChange} />
+                        <Input placeholder='Từ khóa' name='keyword' type='text' handleChange={handleChange} />
+                        <Input placeholder='Thông điệp' name='message' type='text' handleChange={handleChange} />
                         <div className="h-[1px] w-full bg-gray-400 my-2" />
                         {isLoading ? (<Loader />)
                             : (
@@ -99,7 +99,7 @@ const Welcome = () => {
                                     type="button"
                                     onClick={handleSubmit}
                                     className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer">
-                                    Send now
+                                    Gửi ngay
                                 </button>
                             )}
                     </div>
