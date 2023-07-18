372394ea44127beaac9a6cdd528877763f264b77
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2023-07-18 18:35:44 +0700

    ui finish

--- src/components/admin_components/PrizeManagement.jsx
+++ src/components/admin_components/PrizeManagement.jsx
@@ -17,6 +17,7 @@ const PrizeManagement = () => {
     const poolSize = 100;
     const [selectedItems, setSelectedItems] = useState([]);
     const [confirmSelected, setConfirmSelected] = useState(false);
+    const [confirmLottery, setConfirmLottery] = useState(false);
     const [prizePool, setPrizePool] = useState([]);
     const [resultArray, setResultArray] = useState([]);
     const [newItem, setNewItem] = useState({
@@ -33,7 +34,7 @@ const PrizeManagement = () => {
         if (confirmSelected && selectedItems.toString() != [].toString()) {
             let { result_array, initial_value } = calculatePoolProportion({ items_array: selectedItems })
             setResultArray(result_array);
-            setInitValue(initial_value.toFixed(3)*(10**18));
+            setInitValue(initial_value.toFixed(3) * (10 ** 18));
         }
     }, [confirmSelected])
     useEffect(() => {
@@ -43,15 +44,15 @@ const PrizeManagement = () => {
         }
         setPrizePool(prize_pool);
     }, [resultArray, initValue])
-    useEffect(()=>{
+    useEffect(() => {
         let input_proportions = [];
         let input_items = [];
-        for(let i = 0; i < prizePool.length; i++){
+        for (let i = 0; i < prizePool.length; i++) {
             input_items.push(prizePool[i].item.id);
             input_proportions.push(prizePool[i].proportion);
         }
-        addPrize(input_proportions,input_items,initValue);
-    }, [prizePool])
+        addPrize(input_proportions, input_items, initValue);
+    }, [confirmLottery]);
     const [newDocID, setNewDocId] = useState('');
     const handleSubmit = async (type) => {
         let result = await createNewMarketItem(newItem, type);
@@ -151,23 +152,32 @@ const PrizeManagement = () => {
                             }} />
                     </div>
                 ) : ''}
+                {showResult ? (
+                    <div className='py-4'>
+                        <Button color='white' bgColor={currentColor}
+                            text='Xác nhận lựa chọn'
+                            borderRadius='10px' size='md' customFunction={() => {
+                                setConfirmLottery(true);
+                            }} />
+                    </div>
+                ) : ''}
             </div>
             {allItemsShow && !showResult ? (
                 <div className='grid md:grid-cols-2 grid-cols-1'>
                     {savedLotteryItems.map((item, index) => (
-                        <div 
-                        onClick={() =>{
-                            if(!selectedItems.some(obj => obj.id === item.id)){
-                                setSelectedItems((prev) => [...prev, item]);
-                            }
-                            else{
-                                let temp_array = selectedItems.filter(obj => obj.id !== item.id);
-                                setSelectedItems(temp_array);
-                            }
-                        }} 
-                        className='cursor-pointer' key={index}>
+                        <div
+                            onClick={() => {
+                                if (!selectedItems.some(obj => obj.id === item.id)) {
+                                    setSelectedItems((prev) => [...prev, item]);
+                                }
+                                else {
+                                    let temp_array = selectedItems.filter(obj => obj.id !== item.id);
+                                    setSelectedItems(temp_array);
+                                }
+                            }}
+                            className='cursor-pointer' key={index}>
                             <div className="rounded-lg shadow-lg white-glassmorphism max-w-sm flex flex-row p-4 hover:border-green-400"
-                            style={{borderColor: selectedItems.some(obj => obj.id === item.id)? 'green': ''}}>
+                                style={{ borderColor: selectedItems.some(obj => obj.id === item.id) ? 'green' : '' }}>
                                 <img className="rounded-t-lg" src={item.data.image} alt="" width={100} height={100} />
                                 <div className='p-4 flex flex-col'>
                                     <div className="w-100 flex-col flex dark:text-white">
@@ -178,6 +188,34 @@ const PrizeManagement = () => {
                                         <div>
                                             <h6 className='text-xs'>Giá</h6>
                                             <p className='text-sm'>{item.data.price} ETH</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                    )}
+                </div>
+            ) : ''}
+            {showResult ? (
+                <div className='grid md:grid-cols-2 grid-cols-1'>
+                    {prizePool.map((item, index) => (
+                        <div key={index}>
+                            <div className="rounded-lg shadow-lg white-glassmorphism max-w-sm flex flex-row p-4 hover:border-green-400">
+                                <img className="rounded-t-lg" src={item.item.data.image} alt="" width={100} height={100} />
+                                <div className='p-4 flex flex-col'>
+                                    <div className="w-100 flex-col flex dark:text-white">
+                                        <div>
+                                            <h6 className='text-xs'>Tên sản phẩm</h6>
+                                            <p className='text-sm'>{item.item.data.name}</p>
+                                        </div>
+                                        <div>
+                                            <h6 className='text-xs'>Giá</h6>
+                                            <p className='text-sm'>{item.item.data.price} ETH</p>
+                                        </div>
+                                        <div>
+                                            <h6 className='text-xs'>Tỉ lệ</h6>
+                                            <p className='text-sm'>{item.proportion} %</p>
                                         </div>
                                     </div>
                                 </div>
